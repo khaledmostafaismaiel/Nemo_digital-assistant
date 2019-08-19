e05a63a10dbd3d17b92ffa0df566971b04e4e54d
Khaled Mostafa <khaledmostafa297@gmail.com>
2019-08-19 09:23:58 +0200

    upload after some work in reformating

--- rosa_raspberrypi/rosa_jarvis.py
+++ rosa_raspberrypi/rosa_jarvis.py
@@ -25,9 +25,23 @@ engine.setProperty('female', voices[0].gender)
 engine.setProperty(22, voices[0].age)
 
 
-remind_speech = 'global'
+remind_speech = 'still_version'
 
 
+DIGITAL_ASSISTANT_NAME = "Rosa"
+DIGITAL_ASSISTANT_DATE_OF_BIRTH = "15.5.2019"
+DIGITAL_ASSISTANT_GENDER = "female"
+DIGITAL_ASSISTANT_CREATOR_NAME = "khaled mostafa"
+
+CREATOR_NAME = "khaled mostafa"
+CREATOR_FIRST_NAME = "khaled"
+CREATOR_MIDDEL_NAME = "mostafa"
+CREATOR_LAST_NAME = "ismaiel"
+CREATOR_AGE = "26"
+CREATOR_DATE_OF_BIRTH = "1.1.1994"
+CREATOR_GENDER = "male"
+CREATOR_PHONE_NUMBER = "01143325016"
+CREATOR_JOB_TITLE = "Embedded System Software Engineer"
 
 
 
@@ -74,10 +88,10 @@ def say_my_name():
             break
 
 def speak(audio):
-    print('Rosa: ' + audio)  # i add this line to print what bondo2 said
+    print(DIGITAL_ASSISTANT_NAME,':',audio)  # i add this line to print what bondo2 said
     engine.say(audio)
     engine.runAndWait()
-    write_in_database("rosa",audio)
+    write_in_database(DIGITAL_ASSISTANT_NAME,audio)
 
 def welcome_message():
     hour = int(datetime.datetime.now().hour)
@@ -90,7 +104,7 @@ def welcome_message():
     else:
         speak("Good Evening!")
 
-    speak("My name is Rosa. How may I help you ?")
+    speak("My name is "+DIGITAL_ASSISTANT_NAME+". How may I help you ?")
 
 
 def takeCommand():
@@ -106,14 +120,14 @@ def takeCommand():
     try:
         print("Recognizing...")
         query = r.recognize_google(audio, language='en-US')
-        print("You :" ,query)
+        print( CREATOR_FIRST_NAME,':',query)
     # except sr.UnknownValueError :
     except Exception as e:
-        print("You :" ,e)
+        print(CREATOR_FIRST_NAME,':',e)
         print("Say that again please...")
         return "None"
     
-    write_in_database("you",query)
+    write_in_database(CREATOR_FIRST_NAME,query)
     return query
 
 
@@ -124,6 +138,75 @@ def sendEmail(to, content):
     server.login('khaledmostafa297@gmail.com', '01143325016')
     server.sendmail('khaledmostafa297@gmail.com', to, content)
     server.close()
+
+
+
+def compose_email():
+    try:
+        speak("What should I say?")
+        content = takeCommand()
+        #speak("Who is the recipient ?")
+        #to = takeCommand()
+        to = "Khalidgad23@gmail.com"
+        sendEmail(to, content)
+        speak("Email has been sent successfully !")
+    except Exception as e:
+        print(e)
+        speak("Sorry ,I am not able to send this email")
+
+
+
+def open_website(website_url):
+    webbrowser.open(website_url, new=0, autoraise=False)
+
+
+
+YOUTUBE_URL = 'https://www.youtube.com'
+
+GOOGLE_URL = 'https://www.google.com'
+
+STACK_OVERFLOW_URL = 'https://stackoverflow.com/'
+
+LINKED_IN_URL = 'https://www.linkedin.com/in/khaled-mostafa-414265bb/'
+
+GIT_HUB_URL = 'https://github.com/khaledmostafaismaiel'
+
+INSTAGRAM_URL = 'https://www.instagram.com/'
+
+FACEBOOK_URL = 'https://www.facebook.com/khaled.asfora.7'
+
+WHATS_APP_URL = 'https://web.whatsapp.com/'
+
+GOOGLE_TRANSLATION_URL = 'https://translate.google.com'
+
+G_MAIL_URL = 'https://mail.google.com'
+
+EVER_NOTE_URL = 'https://www.evernote.com/client/web#?an=true&n=a28be6fe-bd4b-41e6-84aa-e6fe500444e5&s=s669&/'
+
+GOOGLE_DRIVE_URL = 'https://drive.google.com/drive/my-drive'
+
+PROGRAMIZE_URL = 'https://www.programiz.com/'
+
+
+VISUAL_CODE_STUDIO_DIR = ""
+
+ARDUINO_DIR = ""
+
+PYCHARM_DIR = ""
+
+
+def battery_status():
+    battery = psutil.sensors_battery()
+    plugged = battery.power_plugged
+    percent = int(battery.percent)
+    time_left = secs2hours(battery.secsleft)
+    speak(str(percent))
+    if percent < 40 and plugged == False:
+        speak('sir, please connect charger because i can survive only ' + time_left)
+    if percent < 40 and plugged == True:
+        speak("don't worry,charger is connected")
+    else:
+        speak('no need to connect the charger because i can survive ' + time_left)
 
 
 if __name__ == "__main__":
@@ -147,50 +230,51 @@ while True :
             speak(results)
 
         elif (('open' in query)and('youtube' in query)):
-            webbrowser.open('https://www.youtube.com', new=0, autoraise=False)
+            open_website(YOUTUBE_URL)
 
         elif (('open' in query)and('google' in query)):
-            webbrowser.open('https://www.google.com', new=0, autoraise=False)
+            open_website(GOOGLE_URL)
 
         elif (('open' in query)and('stack over flow' in query)):
-            webbrowser.open('https://stackoverflow.com/', new=0, autoraise=False)
+            open_website(STACK_OVERFLOW_URL)
 
         elif (('open' in query)and('linkedin' in query)):
-            webbrowser.open('https://www.linkedin.com/in/khaled-mostafa-414265bb/', new=0, autoraise=False)
+            open_website(LINKED_IN_URL)
 
         elif (('open' in query)and('github' in query)):
-            webbrowser.open('https://github.com/khaledmostafaismaiel', new=0, autoraise=False)
+            open_website(GIT_HUB_URL)
 
         elif (('open' in query)and('facebook' in query)):
-            webbrowser.open('https://www.facebook.com/khaled.asfora.7', new=0, autoraise=False)
+            open_website(FACEBOOK_URL)
 
         elif (('open' in query)and('instagram' in query)):
-            webbrowser.open('https://www.instagram.com/', new=0, autoraise=False)
+            open_website(INSTAGRAM_URL)
 
         elif (('open' in query)and('whatsapp' in query)):
-            webbrowser.open('https://web.whatsapp.com/', new=0, autoraise=False)
+            open_website(WHATS_APP_URL)
 
         elif (('open' in query)and('gmail' in query)):
-            webbrowser.open('https://mail.google.com', new=0, autoraise=False)
+            open_website(G_MAIL_URL)
 
         elif (('open' in query)and('translate' in query)):
-            webbrowser.open('https://translate.google.com', new=0, autoraise=False)
+            open_website(GOOGLE_TRANSLATION_URL)
 
         elif (('open' in query)and('drive' in query)):
-            webbrowser.open('https://drive.google.com/drive/my-drive', new=0, autoraise=False)
+            open_website(GOOGLE_DRIVE_URL)
 
         elif (('open' in query)and('ever note' in query)):
-            webbrowser.open('https://www.evernote.com/client/web#?an=true&n=a28be6fe-bd4b-41e6-84aa-e6fe500444e5&s=s669&/', new=0, autoraise=False)
+            open_website(EVER_NOTE_URL)
         
-        elif (('open' in query)and('quraan' in query)):
-            webbrowser.open('https://www.youtube.com/watch?v=2rx0DYOex1c', new=0, autoraise=False)
-
         elif (('open' in query)and('programiz' in query)):
-            webbrowser.open('https://www.programiz.com/', new=0, autoraise=False)
+            open_website(PROGRAMIZE_URL)
     
         elif (('open' in query)and('songs' in query)) or (('open' in query)and('song' in query)):
             webbrowser.open('https://www.youtube.com/watch?v=hlznpxNGFGQ&list=RDhlznpxNGFGQ&start_radio=1', new=0, autoraise=False)
             speak('Enjoy !')
+        
+        elif (('open' in query)and('quraan' in query)):
+            webbrowser.open('https://www.youtube.com/watch?v=2rx0DYOex1c', new=0, autoraise=False)
+        
             '''
 
         elif 'music' in query:
@@ -207,10 +291,12 @@ while True :
             speak('Enjoy !')
             os.system(random_music)
             '''
+            '''
 
         elif query == 'play music' or query == 'romantic bollywood music' or query == ' bollywood music':
             speak('Enjoy !')
             playsound('Main_Hoon_Saath_Tere.mp3')
+            '''
             '''
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
@@ -226,15 +312,16 @@ while True :
 
         elif 'day we are' in query:
             speak(time.strftime("%A"))
-
+            '''
         elif 'open code' in query:
             codePath = "bin/code"
             os.startfile(codePath)
-
+            '''
+            '''
         elif 'open pycharm' in query:
             codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
-
+            '''
         elif ((('abort' in query) and ('please' in query))or(('stop' in query) and ('please' in query)) \
              or (('shut down' in query) and ('please' in query))or(('turn off' in query) and ('please' in query))):
             speak('Ok ,bye , see you later .')
@@ -266,7 +353,7 @@ while True :
             speak('my creator try to develop a way to make me do some short talks with humans')
 
         elif (('ok google' in query) or ('hi google' in query) or ('hello google' in query)):
-            speak('i am flatter, but google is not me. my name is rose if you forget it')
+            speak('i am flatter, but google is not me. my name is '+DIGITAL_ASSISTANT_NAME+' if you forget it')
             
         elif (('ok siri' in query) or ('hi siri' in query) or ('hello siri' in query)):
             speak('i am flatter, but siri is not me. my name is rose if you forget it')
@@ -279,7 +366,7 @@ while True :
             speak('you just made my day')
 
         elif (('your best friend' in query) or ('your friend' in query)):
-            speak('khaled is my best friend he is my creator.')
+            speak(CREATOR_FIRST_NAME+' is my best friend he is my creator.')
 
         elif (('you have boyfriend' in query) or ('you have a boyfriend' in query) \
             or ('you have boy friend' in query) or ('you in relationship' in query) or ('you in a relationship' in query)):
@@ -289,45 +376,36 @@ while True :
             speak('i think its complicated , sorry i can\'t')
 
         elif 'who are you' in query or 'about yourself' in query:
-            speak('iam rose and iam a digital assistant and i will be the smartest one as soon as posible,')
-            speak('khaled mostafa is my creator and he start developing me at 15 5 2019')
+            speak('iam'+ DIGITAL_ASSISTANT_NAME+' and iam a digital assistant and i will be the smartest one as soon as posible,')
+            speak(CREATOR_NAME+' is my creator and he start developing me at '+DIGITAL_ASSISTANT_DATE_OF_BIRTH)
 
         elif 'how are you' in query:
             speak('oh')
             speak('iam fine and you ! , how have you been')
-
+            '''
         elif 'logout' in query:
             speak('ok')
             ctypes.windll.user32.LockWorkStation()
-
+            '''
+            '''
         elif 'take screenshot' in query or 'take screen shot' in query or 'snapshot' in query \
         or 'take a screenshot' in query or 'take a screen shot' in query:
-            speak('ok,let me take a screenshot ')
-            speak('check your desktop, i saved there')
+            speak('ok')
             pic = pyautogui.screenshot()
             pic.save('C:/home/robotics')
-
+            speak('check your desktop, i saved there')
+            '''
         elif 'your born' in query or ' you born' in query or 'how old are you' in query or 'your birthday' in query:
-            speak('my creator starts coding me at 15,5,2019')
+            speak('my creator starts coding me at '+DIGITAL_ASSISTANT_DATE_OF_BIRTH)
 
         elif ('who' in query and 'creats you' in query) or ('who' in query and 'your creator' in query):
-            speak('khaled mostafa is my creator')
+            speak(CREATOR_NAME+' is my creator')
 
         elif 'your favourite food' in query:
             speak('a lot of zeros and ones')
 
         elif 'battery' in query  and 'percentage' in query:
-            battery = psutil.sensors_battery()
-            plugged = battery.power_plugged
-            percent = int(battery.percent)
-            time_left = secs2hours(battery.secsleft)
-            speak(str(percent))
-            if percent < 40 and plugged == False:
-                speak('sir, please connect charger because i can survive only ' + time_left)
-            if percent < 40 and plugged == True:
-                speak("don't worry,charger is connected")
-            else:
-                speak('no need to connect the charger because i can survive ' + time_left)
+            battery_status()
 
         elif 'sing song' in query or 'sing a song' in query:
             speak('la la la la la la la ')
@@ -350,23 +428,13 @@ while True :
             '''
         # Ask Reminder
         elif 'reminder' in query:
-            if remind_speech is 'global':
+            if remind_speech is 'still_version':
                 speak('you do not have any reminder for today')
             else:
                 speak('you have one reminder' + remind_speech)
 
         elif 'send email' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                #speak("Who is the recipient ?")
-                #to = takeCommand()
-                to = "Khalidgad23@gmail.com"
-                sendEmail(to, content)
-                speak("Email has been sent successfully !")
-            except Exception as e:
-                print(e)
-                speak("Sorry ,I am not able to send this email")
+            compose_email()
         
         elif 'brightness' in query:
             brightness(query)
